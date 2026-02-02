e411056a7418461a8bf039f31f3af5a4441ef27c
reiveetis <trupinelisltu@gmail.com>
2026-02-02 03:39:20 +0100

    bad implementation of MPI, needs work

--- src/main/java/AppMPJ.java
+++ src/main/java/AppMPJ.java
@@ -4,66 +4,203 @@ import util.Logger;
 
 import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.HexFormat;
 
 
 public class AppMPJ {
     private final static int ROOT = 0;
-    private final static int SHA256_LEN = 64;
-    private final static int MD5_LEN = 32;
-    private final static int MATCH_FOUND_FLAG = 127;
+    private final static int TAG_FORCE_STOP = 0;
+    private final static int TAG_DONE = 1;
+    private final static int TAG_PROGRESS = 2;
 
-    // init buffers with offsets
-
-    // generate hash
-    // check hash
-        // if found, stop everything
-    // add +1 to buffer
-        // check for carryover
+    private final static int OFF_FORCE_STOP = 1;
+    private final static int OFF_DONE = 2;
 
     public static void main(String[] args) {
+        MPI.Init(args);
         // read args to init vars
         int max = 6;
-        BigInteger allPerms = new BigInteger("1004");
-        char[] hash = "71e50ae29377c232b34b79a7b5900c01".toCharArray();
-        char[] charset = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-        MPI.Init(args);
+        byte[] targetBytes = HexFormat.of().parseHex("71e50ae29377c232b34b79a7b5900c01");
+        String charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+        BigInteger allPerms = calculateAllPermutations(1, max, charset.length());
+        HashAlgorithm algorithm = HashAlgorithm.MD5;
 
         int self = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        int[] buffer = new int[max];
-        int[] killswitch = new int[1];
-        Request kill_req;
 
-        // we are expecting a message in the future
-        kill_req = MPI.COMM_WORLD.Irecv(killswitch, 0, 1, MPI.INT, MPI.ANY_SOURCE, 1337);
-        while (killswitch[0] != 1) {
-            try {
-                Thread.sleep(100 + self);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            double rng = Math.random();
-            Logger.debug("<"+self+"> " + rng);
-            if (rng > .95) {
-                Logger.debug("<"+self+"> " + "Match found!");
-                killswitch[0] = 1;
-                // send message
-                for (int i = 0; i < size; i++) {
-                    MPI.COMM_WORLD.Isend(killswitch, 0, 1, MPI.INT, i, 1337);
+        long start = 0;
+        if (self == ROOT) {
+            start = System.currentTimeMillis();
+        }
+
+        Request killReq;
+        Request doneReq;
+        BigInteger chunk;
+        BigInteger chunkRem;
+        BigInteger counter;
+        int[] currentLengthPtr = {0};
+        int maxCh = charset.length();
+
+        int[] buffer;
+        int[] sndBuf = new int[max + 2];
+        int[] rcvBuf = new int[max + 2];
+        int[] killBuf = new int[1];
+
+        // spawn UI
+        // handle kill switch at the same time as progress to save bandwidth
+        if (self == ROOT) {
+            killReq = MPI.COMM_WORLD.Irecv(killBuf, 0, 1, MPI.INT, MPI.ANY_SOURCE, TAG_FORCE_STOP);
+
+            while (killBuf[0] != 1) {
+                // check for kill switch
+                if (killReq.Test() != null) {
+                    // sync buffers
+                    killReq.Wait();
+                    Logger.debug("ROOT GOT KILL SIGNAL!");
+                    for (int i = 0; i < size; i++) {
+                        MPI.COMM_WORLD.Isend(killBuf, 0, 1, MPI.INT, i, TAG_FORCE_STOP);
+                    }
                 }
             }
-            // did we receive message?
-            if (kill_req.Test() != null) {
-                // sync buffers
-                kill_req.Wait();
+        } else {
+            chunk = allPerms.divide(BigInteger.valueOf(size - 1));
+            chunkRem = allPerms.mod(BigInteger.valueOf(size - 1));
+            counter = chunk;
+            if (self == size - 1) {
+                counter = counter.add(chunkRem);
+            }
+            buffer = bigIntToBuffer(chunk.multiply(BigInteger.valueOf(self - 1)), max, currentLengthPtr, maxCh);
+
+            killReq = MPI.COMM_WORLD.Irecv(killBuf, 0, 1, MPI.INT, ROOT, TAG_FORCE_STOP);
+            while (killBuf[0] != 1) {
+                // build bytes[]
+                byte[] bytes;
+                bytes = new byte[currentLengthPtr[0]];
+                int pos = 0;
+                for (int i = max - currentLengthPtr[0]; i <= max - 1; i++) {
+                    bytes[pos] = (byte)charset.charAt(buffer[i]);
+                    pos++;
+                }
+
+//                String str = new String(bytes);
+//                System.out.println(str);
+
+                // match
+                if (matchToTarget(bytes, targetBytes, algorithm)) {
+                    Logger.debug("Match: " + new String(bytes));
+                    killBuf[0] = 1;
+                    MPI.COMM_WORLD.Isend(killBuf, 0, 1, MPI.INT, ROOT, TAG_FORCE_STOP);
+                    break;
+                }
+
+                // increase/decrease
+                buffer[max - 1]++;
+                counter = counter.subtract(BigInteger.ONE);
+                if (counter.compareTo(BigInteger.ZERO) <= 0) {
+                    break;
+                }
+
+                // carryover
+                int i = max - 1;
+                while (i > 0) {
+                    if (buffer[i] == maxCh) {
+                        if (currentLengthPtr[0] == max - i) {
+                            currentLengthPtr[0]++;
+                            resetBuffer(buffer);
+                            break;
+                        }
+                        buffer[i - 1]++;
+                        buffer[i] = 0;
+                        i--;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (killReq.Test() != null) {
+                    // sync buffers
+                    killReq.Wait();
+                }
             }
         }
 
-        Logger.debug("<"+self+"> " + "done!");
 
+        MPI.COMM_WORLD.Barrier();
+        if (self == ROOT) {
+            printStats(System.currentTimeMillis() - start, BigInteger.ZERO);
+        }
+
+//        Logger.debug("Done!");
         MPI.Finalize();
     }
+
+    private static BigInteger calculateAllPermutations(int min, int max, int maxCh) {
+        BigInteger result = BigInteger.ZERO;
+        for (int i = min; i <= max; i++) {
+            result = result.add(BigInteger.valueOf(maxCh).pow(i));
+        }
+        return result;
+    }
+
+    private static void printStats(long time, BigInteger tries) {
+        Logger.info("Finished in: " + time + " ms");
+        Logger.info("Total attempts: " + tries);
+        Logger.info("Hashing speed: " + tries.divide(BigInteger.valueOf(time)).divide(BigInteger.valueOf(1000)) + " MH/s");
+        Logger.info("Shutting down...");
+    }
+
+    private static void resetBuffer(int[] buf) {
+        Arrays.fill(buf, 0);
+    }
+
+    private static boolean matchToTarget(byte[] bytes, byte[] targetBytes, HashAlgorithm algorithm) {
+        byte[] currentHash = Hasher.hash(bytes, algorithm);
+        return Arrays.equals(currentHash, targetBytes);
+    }
+
+    private static int[] bigIntToBuffer(BigInteger num, int max, int[] currentLengthPtr, int maxCh) {
+        int[] result = new int[max];
+        if (num.equals(BigInteger.ZERO)) {
+            currentLengthPtr[0] = 1;
+            return result;
+        }
+
+        // After every carryover, the buffer is set to 0, therefore we just skip those
+        BigInteger skip = BigInteger.valueOf(maxCh);
+        int skipCnt = 0;
+        while (num.compareTo(skip) >= 0) {
+            skipCnt++;
+            num = num.subtract(skip);
+            skip = skip.multiply(BigInteger.valueOf(maxCh));
+        }
+
+        int index = max - 1;
+        while (num.compareTo(BigInteger.ZERO) > 0) {
+            int tmp = num.mod(BigInteger.valueOf(maxCh)).intValue();
+            result[index] = tmp;
+            index--;
+            num = num.divide(BigInteger.valueOf(maxCh));
+        }
+
+        currentLengthPtr[0] = skipCnt + 1;
+        return result;
+    }
+
+    private static BigInteger bufferToBigInt(int[] buf, int max, int[] currentLengthPtr) {
+        BigInteger result = BigInteger.ZERO;
+        for (int i = 1; i < currentLengthPtr[0]; i++) {
+            result = result.add(BigInteger.valueOf(max).pow(i));
+        }
+
+        int exp = 0;
+        for (int i = buf.length - 1; i >= 0; i--) {
+            if (buf[i] > 0) {
+                result = result.add(BigInteger.valueOf(buf[i]).multiply(BigInteger.valueOf(max).pow(exp)));
+            }
+            exp++;
+        }
+
+        return result;
+    }
+
 }
